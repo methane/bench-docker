c333bd0aa0689b58d91bc321f7881bdf795066a3
Inada Naoki <songofacandy@gmail.com>
2020-12-17 16:38:12 +0900

    profiling

--- www/go/bench.go
+++ www/go/bench.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	"encoding/csv"
 	"fmt"
-	"io"
 	"os"
-	"runtime"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/pkg/profile"
 )
 
 // DB取得
@@ -25,7 +25,7 @@ func getDB() *sql.DB {
 	}
 	name := os.Getenv("DB_NAME")
 	if name == "" {
-		name = "db"
+		name = "test"
 	}
 	user := os.Getenv("DB_USER")
 	if user == "" {
@@ -37,7 +37,7 @@ func getDB() *sql.DB {
 	}
 
 	dsn := fmt.Sprintf(
-		"%s%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true",
+		"%s%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true&interpolateParams=true",
 		user, password, host, port, name)
 
 	db, err := sql.Open("mysql", dsn)
@@ -94,19 +94,16 @@ type User struct {
 	UpdatedAt       time.Time
 }
 
-// CSV読み込み＆DBインサート＆CSV書き出し
-func work(db *sql.DB) {
+func work1(db *sql.DB) {
 	var (
 		err    error
 		file   *os.File
 		reader *csv.Reader
 		lines  []string
-		line   string
-		rows   *sql.Rows
 	)
 
 	// CSV読み込み
-	printTime("import CSV start")
+	//printTime("import CSV start")
 	file, err = os.Open("../import_users.csv")
 	if err != nil {
 		panic(err)
@@ -139,7 +136,16 @@ func work(db *sql.DB) {
 			panic(err)
 		}
 	}
-	printTime("import CSV end")
+	//printTime("import CSV end")
+}
+
+// CSV読み込み＆DBインサート＆CSV書き出し
+func work(db *sql.DB) {
+	var (
+		err  error
+		file *os.File
+		rows *sql.Rows
+	)
 
 	rows, err = db.Query("select * from users order by id")
 	if err != nil {
@@ -147,7 +153,7 @@ func work(db *sql.DB) {
 	}
 
 	// CSV書き出し
-	printTime("export CSV start")
+	//printTime("export CSV start")
 
 	file, err = os.Create("./export_users.csv")
 	if err != nil {
@@ -155,8 +161,11 @@ func work(db *sql.DB) {
 	}
 	defer file.Close()
 
+	w := bufio.NewWriterSize(file, 512*1024)
+	defer w.Flush()
+
 	// ヘッダー書き込み
-	_, err = file.WriteString(`"id","name","email","email_verified_at","password","remember_token","created_at","updated_at"` + "\n")
+	_, err = w.WriteString(`"id","name","email","email_verified_at","password","remember_token","created_at","updated_at"` + "\n")
 	if err != nil {
 		panic(err)
 	}
@@ -177,7 +186,8 @@ func work(db *sql.DB) {
 			panic(err)
 		}
 
-		line = fmt.Sprintf(
+		_, err = fmt.Fprintf(
+			w,
 			`%d,"%s","%s","%s","%s","%s","%s","%s"`+"\n",
 			user.ID,
 			user.Name,
@@ -187,59 +197,11 @@ func work(db *sql.DB) {
 			user.RememberToken,
 			user.CreatedAt.Format("2006-01-02 15:04:05"),
 			user.UpdatedAt.Format("2006-01-02 15:04:05"))
-
-		_, err = file.WriteString(line)
 		if err != nil {
 			panic(err)
 		}
 	}
-	printTime("export CSV end")
-
-	// 入力CSVと出力CSVを突合
-	printTime("compare CSV start")
-	var (
-		file1   *os.File
-		file2   *os.File
-		reader1 *csv.Reader
-		reader2 *csv.Reader
-		row1    []string
-		row2    []string
-		err1    error
-		err2    error
-	)
-
-	file1, err1 = os.Open("../import_users.csv")
-	if err1 != nil {
-		panic(err1)
-	}
-	file2, err2 = os.Open("./export_users.csv")
-	if err2 != nil {
-		panic(err2)
-	}
-	defer file1.Close()
-	defer file2.Close()
-
-	reader1 = csv.NewReader(file1)
-	reader2 = csv.NewReader(file2)
-	for {
-		row1, err1 = reader1.Read()
-		row2, err2 = reader2.Read()
-		if err1 == io.EOF && err2 == io.EOF {
-			// 処理終了
-			break
-		}
-		if !(row1[0] == row2[0] &&
-			row1[1] == row2[1] &&
-			row1[2] == row2[2] &&
-			row1[3] == row2[3] &&
-			row1[4] == row2[4] &&
-			row1[5] == row2[5] &&
-			row1[6] == row2[6] &&
-			row1[7] == row2[7]) {
-			panic("入力CSVと出力CSVが一致しません")
-		}
-	}
-	printTime("compare CSV end")
+	//printTime("export CSV end")
 }
 
 // 実行時間を出力
@@ -249,38 +211,18 @@ func printTime(message string) {
 
 // メイン処理
 func main() {
-
-	println("Go " + runtime.Version())
-
 	db := getDB()
 	defer db.Close()
 
-	times := []float64{}
-	for i := 1; i <= 10; i++ {
-		start := time.Now()
-		startNanoTime := start.UnixNano()
-		println(start.Format("2006-01-02 15:04:05.000000"))
+	//initialize(db)
+	//work1(db)
 
-		// 初期化
-		initialize(db)
+	defer profile.Start().Stop()
 
+	start := time.Now()
+	for i := 1; i <= 100; i++ {
 		// 負荷処理
 		work(db)
-
-		end := time.Now()
-		endNanoTime := end.UnixNano()
-		println(end.Format("2006-01-02 15:04:05.000000"))
-
-		// 秒に変換
-		s := float64(endNanoTime-startNanoTime) / float64(1000*1000*1000) // ミリ * マイクロ * ナノ
-		println(fmt.Printf("%f", s))
-		times = append(times, s)
 	}
-	var sum float64
-	for _, s := range times {
-		sum += s
-	}
-	avg := float64(sum / float64(len(times)))
-	println(fmt.Printf("平均秒数：%f", avg))
-
+	fmt.Printf("t=%v\n", time.Now().Sub(start))
 }
